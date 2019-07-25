dd1cb9808f28f9a1d97a4546d3d4cafdabeb9db0
Andrei Turcanu <andriesturcanu@gmail.com>
2019-07-25 15:21:36 +0300

    Bubble sort method in java,change i< (n-j)in bubbleSort method on n

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -3,9 +3,9 @@ public class BubbleSort {
         int n = arr.length;
         int temp;
         for (int i = 0; i < n; i++) {
-            for (int j = 1; j < (n - i); j++) {
-                if (arr[j - 1] > arr[j]) {
+            for (int j = 1; j < n; j++) {
 
+                if (arr[j - 1] > arr[j]) {
                     temp = arr[j - 1];
                     arr[j - 1] = arr[j];
                     arr[j] = temp;
